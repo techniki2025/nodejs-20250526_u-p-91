27508adfaf9814c4ee8ad9e5ea814c9baf62724a
PavelYudin <u-p.91@mail.ru>
2025-06-02 21:38:19 +0300

    1st task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,3 +1,7 @@
 export default function sum(a, b) {
-  /* ваш код */
+  if(typeof a === 'number' && typeof b === 'number') {
+    return a + b;
+  } else {
+    throw new TypeError('TypeError');
+  }
 }
