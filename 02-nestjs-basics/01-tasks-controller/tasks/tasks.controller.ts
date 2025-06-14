681587111b59cd7a688e8f55da55dd99fe46bacf
PavelYudin <u-p.91@mail.ru>
2025-06-14 17:16:32 +0300

    2st task

--- 02-nestjs-basics/01-tasks-controller/tasks/tasks.controller.ts
+++ 02-nestjs-basics/01-tasks-controller/tasks/tasks.controller.ts
@@ -9,23 +9,34 @@ import {
 } from "@nestjs/common";
 import { TasksService } from "./tasks.service";
 import { Task } from "./task.model";
+import { TaskDto } from "./task.dto";
 
 @Controller("tasks")
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Get()
-  getAllTasks() {}
+  getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
 
   @Get(":id")
-  getTaskById(@Param("id") id: string) {}
+  getTaskById(@Param("id") id: string) {
+    return this.tasksService.getTaskById(id);
+  }
 
   @Post()
-  createTask(@Body() task: Task) {}
+  createTask(@Body() task: TaskDto): Task {
+    return this.tasksService.createTask(task);
+  }
 
   @Patch(":id")
-  updateTask(@Param("id") id: string, @Body() task: Task) {}
+  updateTask(@Param("id") id: string, @Body() task: TaskDto) {
+    return this.tasksService.updateTask(id, task);
+  }
 
   @Delete(":id")
-  deleteTask(@Param("id") id: string) {}
+  deleteTask(@Param("id") id: string) {
+    return this.tasksService.deleteTask(id);
+  }
 }
--- 02-nestjs-basics/01-tasks-controller/tasks/tasks.service.ts
+++ 02-nestjs-basics/01-tasks-controller/tasks/tasks.service.ts
@@ -4,14 +4,41 @@ import { Task } from "./task.model";
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
+  private keyTask: string = '1';
 
-  getAllTasks(): Task[] {}
+  private findIndexTask(id: string) {
+    return this.tasks.findIndex( task => task.id == id);
+  };
 
-  getTaskById(id: string): Task {}
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
 
-  createTask(task: Task): Task {}
+  getTaskById(id: string): Task {
+    const task = this.tasks.find( task => task.id == id);
+    if(!task) throw new NotFoundException(`No task ${id}`);
+    return task;
+  }
 
-  updateTask(id: string, update: Task): Task {}
+  createTask(task: Task): Task {
+    const id = this.keyTask;
+    task.id = id;
+    this.tasks.push(task);
+    this.keyTask = +id + 1 + '';
+    return task;
+  }
 
-  deleteTask(id: string): Task {}
+  updateTask(id: string, update: Task): Task {
+    const indexTask = this.findIndexTask(id);
+    if(indexTask === -1) throw new NotFoundException(`No task ${id}`);
+    this.tasks[indexTask] = update;
+    this.tasks[indexTask].id = id;
+    return this.tasks[indexTask];
+  }
+
+  deleteTask(id: string): Task {
+    const indexTask = this.findIndexTask(id);
+    if(indexTask === -1) throw new NotFoundException(`No task ${id}`);
+    return this.tasks.splice(indexTask, 1)[0];
+  }
 }
